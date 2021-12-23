74e239388e1f3eb138f46d22651cf3718e25105c
Ivan Mondragon <ivanmondragon42@gmail.com>
2021-12-22 19:52:22 -0600

    Day 9

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -3,6 +3,7 @@ use std::collections::BTreeSet;
 const WIDTH: usize = 100;
 const HEIGHT: usize = 100;
 const SIZE: usize = WIDTH * HEIGHT;
+
 fn main() {
     // initital state set up
     let grid: Vec<u8> = include_str!("../input.txt")
@@ -31,16 +32,33 @@ fn main() {
 
     let low_score: usize = low_points.clone().map(|(_, &n)| n as usize + 1).sum();
 
-    let basins = low_points.map(|(i, &v)| {
-        let higher_adjacent = |(i, v)| adjacent(i).filter(move |&(_, n)| n < 9 && v < n);
-        let mut basin = BTreeSet::from([i]);
+    let basins: Vec<_> = low_points
+        .map(|(i, &v)| {
+            let higher_adjacent = |(i, v)| adjacent(i).filter(move |&(_, n)| n < 9 && v < n);
+            let mut basin = BTreeSet::from([i]);
 
-        let mut to_visit: Vec<_> = higher_adjacent((i, v)).collect();
-        while let Some((i, v)) = to_visit.pop() {
-            basin.insert(i);
-            // to_visit.push((1,1));
-        }
-    });
+            let mut to_visit: Vec<_> = higher_adjacent((i, v)).collect();
+            while let Some((i, v)) = to_visit.pop() {
+                let is_new = basin.insert(i);
+                if is_new {
+                    to_visit.extend(higher_adjacent((i, v)));
+                }
+            }
+
+            basin
+        })
+        .collect();
+
+    dbg!(&basins);
+
+    let sizes = {
+        let mut v: Vec<_> = basins.iter().map(|s| s.len()).collect();
+        v.sort();
+        v
+    };
+
+    let score: usize = sizes[sizes.len() - 3..].iter().product();
 
     println!("Day 9.1: {}", low_score);
+    println!("Day 9.1: {}", score);
 }
